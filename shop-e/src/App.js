ccb304af2d0644e7a79435c5e9e8fb1314b7b369
Ayush Kumar Bhadani <ayushkumarbhadani@gmail.com>
2022-05-28 21:39:27 +0530

    route setup done in shop-e

--- shop-e/src/App.js
+++ shop-e/src/App.js
@@ -1,11 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
+import Products from "./components/Products";
 import { Link } from "react-router-dom";
 import Nav from "./components/Navbar";
 function App() {
   return (
     <BrowserRouter>
       <Nav activeClassName="active" />
+      <Routes>
+        <Route path="/" element={<div><h1>Home</h1></div>} />
+        <Route path="/products" element={<Products />} />
+        <Route path="*" element={<div><h1>404! Not Found</h1></div>} />
+      </Routes>
     </BrowserRouter>
   );
 }
---
+++ shop-e/src/components/Products.js
@@ -0,0 +1,8 @@
+const Products = () => {
+    return (
+        <div>
+            <h1>Hello, I'm from Products</h1>
+        </div>
+    );
+}
+export default Products;
