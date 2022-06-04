c5238553fa8b9111eddc640d82340ba77b3c0356
Ayush Kumar Bhadani <ayushkumarbhadani@gmail.com>
2022-06-04 23:55:34 +0530

    working on user Login

--- shop-e/src/App.js
+++ shop-e/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import './App.css';
 // import { Link } from "react-router-dom";
 import Nav from "./components/Navbar";
@@ -6,16 +7,19 @@ import Products from "./components/Products";
 import ProductDetail from "./components/ProductDetail";
 import Login from "./components/Login";
 function App() {
+  const [user, setUser] = useState();
   return (
     <BrowserRouter>
       <Nav activeClassName="active" />
-      <Routes style={{ marginTop: "60px" }}>
-        <Route path="/" element={<div><h1>Home</h1></div>} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/products/:name/details/:id" element={<ProductDetail />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<div><h1>404! Not Found</h1></div>} />
-      </Routes>
+      <main>
+        <Routes style={{ marginTop: "60px" }}>
+          <Route path="/" element={<div><h1>Home</h1></div>} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:name/details/:id" element={<ProductDetail />} />
+          <Route path="/login" element={<Login setUser={setUser} user={user} />} />
+          <Route path="*" element={<div><h1>404! Not Found</h1></div>} />
+        </Routes>
+      </main>
     </BrowserRouter>
   );
 }
--- shop-e/src/components/Login.js
+++ shop-e/src/components/Login.js
@@ -1,17 +1,41 @@
+import { useState, useNavigation } from "react";
 import "./login.css";
-const Login = () => {
+const Login = ({ setUser, user }) => {
+    const [isLoggedIn, setIsLoggedIn] = useState()
+    const [tempUserId, setTempUserId] = useState();
+    const [tempUserPassword, setTempUSerPassword] = useState();
+    const loginUser = (e, setUser) => {
+        e.preventDefault();
+        if (tempUserId === "test" && tempUserPassword === "Test1234") {
+            setUser({ name: "Test User", id: "1" });
+            return;
+        }
+        else {
+            alert("Incorrect Email or Password");
+        }
+    }
+    if (user) {
+        // useNavigation("/");
+        alert("User LoggedIn");
+    }
     return (
         <section className="form-wrapper">
-            <form>
-                <label for="email">Email
-                    <input type="text" placeholder="Email" />
+            <form className="login-form" onSubmit={(e) => loginUser(e, setUser)}>
+                <h1 className="login-form-heading">Login</h1>
+                <label>Email:
+                    <input type="text" placeholder="Enter Your Email" onChange={(e) => setTempUserId(e.target.value)} />
                 </label>
-                <label> Password
-                    <input type="password" placeholder="password" />
+                <label> Password:
+                    <input type="password" placeholder="Enter Your Password" onChange={(e) => setTempUSerPassword(e.target.value)} />
                 </label>
+                <div>
+                    <input type="submit" value="Login" className="primary-btn submit-btn" />
+                </div>
             </form>
         </section>
     );
+
+
 }
 
 export default Login;
