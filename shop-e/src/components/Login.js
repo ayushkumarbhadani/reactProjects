5cb17e66702852f328f30ca7e8c19cc0ebc5329f
Ayush Kumar Bhadani <ayushkumarbhadani@gmail.com>
2022-06-06 19:52:51 +0530

    updated css and login.js

--- shop-e/src/components/Login.js
+++ shop-e/src/components/Login.js
@@ -5,9 +5,13 @@ const Login = ({ setUser }) => {
     const navigate = useNavigate();
     const [tempUserEmail, setTempUserEmail] = useState();
     const [tempUserPassword, setTempUSerPassword] = useState();
+    const [isUserNameCorrect, setIsUserNameCorrect] = useState(true);
     const loginUser = (e, setUser) => {
         e.preventDefault();
-        if (tempUserEmail !== "test" && tempUserPassword !== "Test1234") return;
+        if (tempUserEmail !== "demo@example.com" && tempUserPassword !== "demo") {
+            setIsUserNameCorrect(false);
+            return;
+        }
 
         setUser({ email: tempUserEmail, name: "Demo User" })
         navigate('/dashboard');
@@ -18,16 +22,30 @@ const Login = ({ setUser }) => {
             <form className="login-form" onSubmit={(e) => loginUser(e, setUser)}>
                 <h1 className="login-form-heading">Login</h1>
                 <label>Email:
-                    <input type="text" placeholder="Enter Your Email" onChange={(e) => setTempUserEmail(e.target.value)} />
+                    <input type="text" placeholder="Enter Your Email" onChange={(e) => setTempUserEmail(e.target.value)} className={!isUserNameCorrect ? "incorrect-auth" : null} />
+
                 </label>
                 <label> Password:
-                    <input type="password" placeholder="Enter Your Password" onChange={(e) => setTempUSerPassword(e.target.value)} />
+                    <input type="password" placeholder="Enter Your Password" onChange={(e) => setTempUSerPassword(e.target.value)} className={!isUserNameCorrect ? "incorrect-auth" : null} />
                 </label>
+                {!isUserNameCorrect && <p>
+                    <span className="incorrect-auth-alert">Incorrect Email or Password</span>
+                </p>}
                 <div>
                     <input type="submit" value="Login" className="primary-btn submit-btn" />
                 </div>
+
             </form>
-        </section>
+            <p className="demo-notice">
+                This is a demo react application. You can login into demo account using:
+                <br />
+                <br />
+                <strong>Email: demo@example.com</strong>
+                <br />
+                <br />
+                <strong>Password: demo</strong>
+            </p>
+        </section >
     );
 
 
