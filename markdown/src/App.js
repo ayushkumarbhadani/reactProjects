bfba8467811aa66c0e1f97bd05ef8c90b2279ab0
Ayush Kumar Bhadani <ayushkumarbhadani@gmail.com>
2022-06-10 09:58:20 +0530

    markdown text converter project

---
+++ markdown/src/App.js
@@ -0,0 +1,98 @@
+import "./App.css";
+import Markdown from "react-markdown";
+import rehypeRaw from "rehype-raw";
+import {useState} from "react";
+import remarkGfm from 'remark-gfm'
+const App=()=>{
+  const [markdownText,setMarkdownText]=useState();
+  return(
+    <main>
+      <textarea className="main-textarea" placeholder="Write your markdown text here.." value={markdownText} onChange={(e)=>{setMarkdownText(e.target.value)}}></textarea>
+      <article className="markdown-output">
+        <Markdown remarkPlugins={[[remarkGfm, {singleTilde: false}]]}  rehypePlugins={[rehypeRaw]}>{markdownText}</Markdown>
+      </article>
+    </main>
+  );
+}
+export default App;
+
+
+
+
+/* 
+
+Headers
+# h1
+## h2
+### h3
+#### h4
+##### h5
+###### h6
+Header 1
+========
+Header 2
+--------
+Emphasis
+*italic*
+_italic_
+**bold**
+__bold__
+***bold italic***
+___bold italic___
+~~strikethrough~~
+`code`
+Lists
+* Item 1
+* Item 2
+- Item 1
+- Item 2
+- [ ] Checkbox off
+- [x] Checkbox on
+1. Item 1
+2. Item 2
+
+Links
+[link](http://google.com)
+[link][google]
+[google]: http://google.com
+<http://google.com>
+Images
+![Image alt text](https://source.unsplash.com/random)
+![Image alt text](https://source.unsplash.com/random "Random Unsplash Imgage")
+![Random Unsplash Image][https://source.unsplash.com/random]
+[https://source.unsplash.com/random]: https://source.unsplash.com/random
+
+Code
+`inline code`
+    4 space indent
+    makes a code block
+```
+code fences
+```
+```js
+codeFences.withLanguage()
+```
+Blockquotes
+> This is
+> a blockquote
+>
+> > Nested
+> > Blockquote
+Horizontal line
+----
+****
+Tables
+| Column 1 Heading | Column 2 Heading |
+| ---------------- | ---------------- |
+| Some content     | Other content    |
+Column 1 Heading | Column 2 Heading
+--- | ---
+Some content | Other content
+
+
+
+
+<input type="text"/>
+
+<input type="reset"/>
+*/
