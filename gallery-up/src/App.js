24b121feb6e91a11c5bcdca730d59398065f8fe4
Ayush Kumar Bhadani <ayushkumarbhadani@gmail.com>
2022-06-12 21:42:17 +0530

    added static data and completed layout

--- gallery-up/src/App.js
+++ gallery-up/src/App.js
@@ -1,24 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState,useEffect} from "react";
+import {FcSearch} from "react-icons/fc";
+import {FiDownload} from "react-icons/fi";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App=()=>{
+  const [data,setData]=useState(null);
+  const [search,setSearch]=useState("");
+  const fetchData=async(URL)=>{
+    const res=await fetch(URL);
+    const upcommingData= await res.json();
+    console.log(res);
+    console.log(upcommingData);
+  }
+  useEffect(()=>{
+    // fetchData(`https://api.unsplash.com/photos/?client_id=${process.env.REACT_APP_API}`);
+
+  },[]);
+  return(
+    <>
+    <nav>
+      <div className="serach-wrapper">
+        <input type="search" value={search} onChange={(e)=>setSearch(e.target.value)}/>
+        <button><FcSearch/></button>
+      </div>
+    </nav>
+    <main className="image-main-container">
+      <section className="row-1">
+
+        <div className="img-container">
+          <img src="https://images.unsplash.com/photo-1655043126269-d93dc7917a1e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3087&q=80" alt="random img"/>
+          <div className="img-desc">
+            <div className="creater-profile">
+              <a href="#">
+              <img src="https://images.unsplash.com/profile-1650865374146-e22aed4e040fimage?ixlib=rb-1.2.1&crop=faces&fit=crop&w=128&h=128"/>
+              <span>Creater Name</span>
+              </a>
+            </div>
+            <a href="#" download="true"><FiDownload/></a>
+          </div>
+        </div>
+
+        
+        <div className="img-container">
+          <img src="https://images.unsplash.com/photo-1655041138326-f25a82627848?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" alt="random img"/>
+        </div>
+        <div className="img-container">
+          <img src="https://images.unsplash.com/photo-1655012325191-cbc22182fa9f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1624&q=80" alt="random img"/>
+        </div>
+        <div className="img-container">
+          <img src="https://images.unsplash.com/photo-1655012325191-cbc22182fa9f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1624&q=80" alt="random img"/>
+        </div>
+        </section>
+
+        <section className="row-2">
+        <div className="img-container">
+          <img src="https://images.unsplash.com/photo-1655043126269-d93dc7917a1e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3087&q=80" alt="random img"/>
+        </div>
+        <div className="img-container">
+          <img src="https://images.unsplash.com/photo-1655041138326-f25a82627848?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" alt="random img"/>
+        </div>
+        <div className="img-container">
+          <img src="https://images.unsplash.com/photo-1655012325191-cbc22182fa9f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1624&q=80" alt="random img"/>
+        </div>
+        <div className="img-container">
+          <img src="https://images.unsplash.com/photo-1655012325191-cbc22182fa9f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1624&q=80" alt="random img"/>
+        </div>
+        </section>
+
+        <section className="row-3">
+        <div className="img-container">
+          <img src="https://images.unsplash.com/photo-1655043126269-d93dc7917a1e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3087&q=80" alt="random img"/>
+        </div>
+        <div className="img-container">
+          <img src="https://images.unsplash.com/photo-1655041138326-f25a82627848?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" alt="random img"/>
+        </div>
+        <div className="img-container">
+          <img src="https://images.unsplash.com/photo-1655012325191-cbc22182fa9f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1624&q=80" alt="random img"/>
+        </div>
+        <div className="img-container">
+          <img src="https://images.unsplash.com/photo-1655012325191-cbc22182fa9f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1624&q=80" alt="random img"/>
+        </div>
+        </section>
+    </main>
+    </>
   );
 }
 
